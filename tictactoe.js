a6627731d47f07fa9e17ef835248b92a4343c412
Jaden <jenzbr@gmail.com>
2021-11-17 01:54:17 -0600

    Added check for win and tie, did a bit more styling, added a reset button

--- tictactoe.js
+++ tictactoe.js
@@ -1,24 +1,28 @@
 let clickedFlag = false;
 
 const gameBoard = (() => { //module
-    const boardArray = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+    const board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
     const generateBoard = () => {
         const boardContainer = document.querySelector('.boardContainer');
-        for (let i = 0; i < (boardArray.length); i++){
+        for (let i = 0; i < (board.length); i++){
             let div = document.createElement('div');
             div.setAttribute('class', 'gridDiv')
-            div.innerHTML = boardArray[i];
+            div.innerHTML = board[i];
             boardContainer.appendChild(div);
         }
     }
+    const updateBoard = (index, player) => {
+        board[index] = player
+        console.log(board)
+    }
+
     return {
-        boardArray,
+        board,
         generateBoard,
+        updateBoard,
     };
 })();
 
-// just added data indeces, need to update the array using data index in order
-//to make a function called after the if else statements to check for a winner
 
 const gameController = (() => { //module
     const markSpace = () => {
@@ -30,18 +34,62 @@ const gameController = (() => { //module
                     if (clickedFlag === false){
                         clickedFlag = true;
                         element.innerText = 'X'
+                        gameBoard.updateBoard(index, 'X')
                     } else {
                         clickedFlag = false;
                         element.innerText = 'O'
+                        gameBoard.updateBoard(index, 'O')
                     }
+                    if (checkWin('X')){
+                        console.log('X wins!');
+                    }
+                    else if (checkWin('O')){
+                        console.log('O wins!');
+                    }
+                    else {
+                        checkTie();
+                    }
+                    
                 }
             });
         })
     }
-    const resetBoard = () => console.log('idk') //makes all the array spaces blank again
+    const resetBoard = () => { //makes all the array spaces blank again
+        gameBoard.board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+        let div = document.querySelectorAll('.gridDiv');
+        div.forEach((element) => {
+            element.innerText = ' ';
+        });
+    }
+
+    const btn = document.querySelector('#btn');
+    btn.addEventListener('click', resetBoard);
+
+    const checkTie = () => {
+       const blankSpace = (element) => element === ' ';
+       if (!gameBoard.board.some(blankSpace)) {
+           console.log('Tie game!');
+       }
+    } 
+    const  checkWin = (player) => {
+        // player = 'X' or 'O'
+      
+        const horizontal = [0,3,6].map(i=>{return[i,i+1,i+2]});
+        const vertical = [0,1,2].map(i=>{return[i,i+3,i+6]});
+        const diagonal = [[0,4,8],[2,4,6]];
+      
+        var allwins = [].concat(horizontal).concat(vertical).concat(diagonal);
+        let res = allwins.some(indices => { 
+            return gameBoard.board[indices[0]] == player && 
+                   gameBoard.board[indices[1]] == player && 
+                   gameBoard.board[indices[2]] == player})
+        return res;
+      }
+
     return {
         markSpace,
         resetBoard,
+        checkWin,
     };
 })();
 
@@ -53,3 +101,7 @@ const Player = (name) => { //factory function
 gameBoard.generateBoard();
 gameController.markSpace();
 
+//TODO
+// Allow input of player names
+// Set up a display element that announces the winning player
+// Style more if I care
