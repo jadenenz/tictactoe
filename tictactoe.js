d879bcf5aa6727fdff2c7d8ffb6b178dfd6af841
Jaden <jenzbr@gmail.com>
2021-11-17 12:08:09 -0600

    Made display for turn/winner, fixed bug with several board instance states, styled more, finished basically

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,5 @@
-let clickedFlag = false;
-
 const gameBoard = (() => { //module
-    const board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+    let board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
     const generateBoard = () => {
         const boardContainer = document.querySelector('.boardContainer');
         for (let i = 0; i < (board.length); i++){
@@ -13,63 +11,76 @@ const gameBoard = (() => { //module
     }
     const updateBoard = (index, player) => {
         board[index] = player
-        console.log(board)
+        return board;
+    }
+
+    const resetBoard = () => { //makes all the array spaces blank again
+        board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+        let div = document.querySelectorAll('.gridDiv');
+        div.forEach((element) => {
+            element.innerText = ' ';
+        });
+        return board;
+    }
+
+    const returnBoard = () => {
+        return board;
     }
 
     return {
         board,
         generateBoard,
         updateBoard,
+        resetBoard,
+        returnBoard
     };
 })();
 
 
 const gameController = (() => { //module
+    let activePlayer = 'X';
+    let gameOver = false;
+    let winnerDisplay = document.querySelector('.winnerDisplay');
     const markSpace = () => {
         let div = document.querySelectorAll('.gridDiv');
         div.forEach((element,index) => {
             element.setAttribute('data-index', index)
             element.addEventListener('click', function(e)  {
                 if (element.innerText === '') {
-                    if (clickedFlag === false){
-                        clickedFlag = true;
+                    if (activePlayer === 'X' && gameOver === false){
+                       setActivePlayer('O');
                         element.innerText = 'X'
                         gameBoard.updateBoard(index, 'X')
-                    } else {
-                        clickedFlag = false;
+                    } else if (activePlayer === 'O' && gameOver === false){
+                        setActivePlayer('X');
                         element.innerText = 'O'
                         gameBoard.updateBoard(index, 'O')
                     }
                     if (checkWin('X')){
                         console.log('X wins!');
+                        winnerDisplay.innerText = 'Player X Wins!'
+                        gameOver = true;
                     }
                     else if (checkWin('O')){
                         console.log('O wins!');
+                        winnerDisplay.innerText = 'Player O wins!'
+                        gameOver = true;
                     }
-                    else {
-                        checkTie();
+                    else if(checkTie()){
+                        console.log('Tie game!');
+                        winnerDisplay.innerText = 'Tie game!'
+                        gameOver = true;
                     }
                     
                 }
             });
         })
     }
-    const resetBoard = () => { //makes all the array spaces blank again
-        gameBoard.board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
-        let div = document.querySelectorAll('.gridDiv');
-        div.forEach((element) => {
-            element.innerText = ' ';
-        });
-    }
 
-    const btn = document.querySelector('#btn');
-    btn.addEventListener('click', resetBoard);
 
     const checkTie = () => {
-       const blankSpace = (element) => element === ' ';
-       if (!gameBoard.board.some(blankSpace)) {
-           console.log('Tie game!');
-       }
+       const blankSpace = (element) => element === ' '; 
+       return (!gameBoard.returnBoard().some(blankSpace)) // returns true if there are no blank spaces
     } 
     const  checkWin = (player) => {
         // player = 'X' or 'O'
@@ -80,16 +91,35 @@ const gameController = (() => { //module
       
         var allwins = [].concat(horizontal).concat(vertical).concat(diagonal);
         let res = allwins.some(indices => { 
-            return gameBoard.board[indices[0]] == player && 
-                   gameBoard.board[indices[1]] == player && 
-                   gameBoard.board[indices[2]] == player})
+            return gameBoard.returnBoard()[indices[0]] == player && 
+                   gameBoard.returnBoard()[indices[1]] == player && 
+                   gameBoard.returnBoard()[indices[2]] == player})
+        if (res)           
         return res;
       }
 
+      
+    const resetGame = () => {
+        gameBoard.resetBoard();
+        gameOver = false;
+        setActivePlayer('X');
+    }
+
+    const btn = document.querySelector('#btn');
+    btn.addEventListener('click', resetGame);
+
+    const setActivePlayer = (player) => { //player is "x" or "o"
+        activePlayer = player; 
+        winnerDisplay.innerText = `Player ${player}'s turn`
+        // winnerDisplay.innerText = 'Player '+player+' has won!'
+    }
+        
     return {
         markSpace,
-        resetBoard,
         checkWin,
+        gameOver,
+        resetGame,
+        setActivePlayer,
     };
 })();
 
@@ -100,8 +130,4 @@ const Player = (name) => { //factory function
 
 gameBoard.generateBoard();
 gameController.markSpace();
-
-//TODO
-// Allow input of player names
-// Set up a display element that announces the winning player
-// Style more if I care
+gameController.setActivePlayer('X')
